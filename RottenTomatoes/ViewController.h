40d53bbb5b3a7661b2142214b96e6e47a0c8aa8b
Chad Jewsbury <cjewsbury@groupon.com>
2015-10-20 20:48:02 -0700

    Initial Commit

---
+++ RottenTomatoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RottenTomatoes
+//
+//  Created by Chad Jewsbury on 10/20/15.
+//  Copyright © 2015 Chad Jewsbury. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
